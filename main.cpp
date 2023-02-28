08fed37e756307b8139810a7cf27e47da505ec6f
Sina Ghasemi <numtest447@gmail.com>
2023-03-01 00:42:40 +0330

    checking activity

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
     manager.createPlayers(5);
     manager.distributeTerritories();
 
-//    foreach (Player* player, manager.getPlayers()) {
-//        foreach (Territory* territory, player->getTerritories()) {
-//            qDebug() << territory->getName() << territory->getOwnerNumber();
-//        }
-//    }
+    foreach (Player* player, manager.getPlayers()) {
+        foreach (Territory* territory, player->getTerritories()) {
+            qDebug() << territory->getName() << territory->getOwnerNumber();
+        }
+    }
 
     return a.exec();
 }
